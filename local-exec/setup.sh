603b52f7bcfcdb46cf270cd5f7a0bb6397405d61
magicalthinking <support@magicalthinking.se>
2019-02-06 00:07:37 +0100

    Updated

--- local-exec/setup.sh
+++ local-exec/setup.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+gcloud beta config set project $3
+
 gcloud beta container clusters get-credentials $1 \
     --region $2 \
     --project $3
 
-kubectl config current-context
-kubectl config use-context $1
-
 kubectl create ns tiller
 kubectl create -f ./local-exec/setup.yaml
+
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo update
 
 helm init \
 	--service-account tiller \
 	--tiller-namespace tiller \
 	--wait
-
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm repo update
 
 # Install ingress
 helm fetch --version 0.30.0 stable/nginx-ingress --untar
